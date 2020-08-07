14e172a7c0ba1ce751c241491ea0c334beccae75
cafe-tera <jorge.eff.96@gmail.com>
2020-08-06 22:24:13 -0400

    api crud equipamientos.services

--- src/services/equipamientos.services.js
+++ src/services/equipamientos.services.js
@@ -3,19 +3,57 @@ import axios from 'axios';
 const API_URL = "https://isw-rifeco-equipamientos.herokuapp.com"
 
 
-const getEquipamientos = () => {
+const getEquipamientos = async () =>{
   let url = `${API_URL}/equipamientos/`;
-  return axios.get(url);
+  return axios.get(url)
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  });
 }
 
-const getEquipamientosById = (id) => {
+const getEquipamientosById = async (id) => {
   let url = `${API_URL}/equipamientos/${id}`;
-  return axios.get(url);
+  return axios.get(url)
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  });
+}
+
+const crearEquipamiento = async (equipamiento) => {
+  let url = `${API_URL}/equipamientos/`;
+  return axios.post(url, { equipamiento })
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  });
+}
+
+const crearEquipamiento = async (equipamiento, id) => {
+  let url = `${API_URL}/equipamientos/${id}`;
+  return axios.put(url, { equipamiento })
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  });
+}
+
+
+const eliminarEquipamiento = async (id) => {
+  let url = `${API_URL}/equipamientos/${id}`;
+  return axios.delete(url)
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  });
 }
 
 const equipamientosService = {
     getEquipamientos,
     getEquipamientosById,
+    crearEquipamiento,
+    eliminarEquipamiento,
 };
 
 export default equipamientosService;
