1916cf986066d24ade6d45bf098763c13a46c48c
Pascal <pascal.arriagada@sansano.usm.cl>
2020-08-07 00:33:50 -0400

    added dialog for editing items on PabellonesPage.jsx. Doing: working in editing form

--- src/pages/PabellonesPage.jsx
+++ src/pages/PabellonesPage.jsx
@@ -1,12 +1,35 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import { blue } from '@material-ui/core/colors';
 
 import pabellonesService from '../services/pabellones.services';
+
+function SimpleDialog(props) {
+    // const classes = useStyles();
+    const { onClose, selectedValue, open } = props;
+  
+    const handleClose = () => {
+      onClose(selectedValue);
+    };
+  
+    const handleListItemClick = (value) => {
+      onClose(value);
+    };
+  
+    return (
+      <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+        <DialogTitle id="simple-dialog-title">Editar pabellon</DialogTitle>
+      </Dialog>
+    );
+  }
 
 const PabellonesPage = () => {
 
     const [pabellones, setPabellones] = useState([]);
+    const [open, setOpen] = React.useState(false);
     useEffect(() => {
-        pabellonesService.getPabellones().then(res =>{
+        pabellonesService.getPabellones().then(res => {
             setPabellones(res.data);
             console.log(res);
         }).catch(error => {
@@ -14,26 +37,51 @@ const PabellonesPage = () => {
         });
     }, []);
 
-    
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (value) => {
+        setOpen(false);
+    };
+
+
     const pabellonesItems = pabellones.map((pabellon) =>
-        <tr key={pabellon.id}>
-            <td> {pabellon.nombre ?? "campo nulo"} </td>
-            <td> {pabellon.capacidad ?? "campo nulo"} </td>
-            <td> {pabellon.ubicacion ?? "campo nulo"} </td>
+        <tr>
+            <td>{pabellon.nombre ?? "campo nulo"}</td>
+            <td>{pabellon.ubicacion ?? "campo nulo"}</td>
+            <td>{pabellon.estado ?? "no asignado"}</td>
+            <td>
+                <button class="btn btn-warning" onClick={handleClickOpen}>
+                    Editar
+                </button>
+            </td>
+            <td>
+                <a href="/pabellones/" class="btn btn-danger">
+                    Eliminar
+                </a>
+            </td>
         </tr>
     );
-
-    return(
-        <div>
-            <div>Lista de todas las Equipamientos</div>
-            <table class="egt">
-                <tr>
-                    <td>Nombre</td>
-                    <td>Capacidad</td>
-                    <td>Ubicacion</td>
-                </tr>
-                {pabellonesItems}
+    return (
+        <div class="col-12">
+            <h1>Pabellones - <a href="/pabellones/agregar" class="btn btn-primary mb-2">Agregar</a>
+            </h1>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Ubicacion</th>
+                        <th>Estado</th>
+                        <th>Editar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {pabellonesItems}
+                </tbody>
             </table>
+            <SimpleDialog open={open} onClose={handleClose} />
         </div>
     )
 }
